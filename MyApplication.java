60cf6edec682e178e521d00afb8d2e473a7f443e
Nurali2508 <250291@astanait.edu.kz>
2025-12-21 23:33:39 +0500

    Update MyApplication.java

--- MyApplication.java
+++ MyApplication.java
@@ -1,30 +1,30 @@
+import Models.Point;
 import Models.Shape;
 import java.io.File;
-import Models.Point;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Locale;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class MyApplication {
+
+class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
-
         File file = new File("C:\\Users\\Nurali\\IdeaProjects\\Assignhment st 2508\\src\\Source");
 
-        Scanner sc = new Scanner(file).useLocale(Locale.US);
+        Scanner sc = new Scanner(file);
         Shape shape = new Shape(new ArrayList<>());
 
-        while(sc.hasNextDouble()) {
-            double x = sc.nextDouble();
-            double y = sc.nextDouble();
+        while (sc.hasNext()) {
+            double x = Double.parseDouble(sc.next());
+            double y = Double.parseDouble(sc.next());
 
             Point point = new Point(x, y);
             shape.addPoint(point);
         }
-        sc.close();
 
-        System.out.println(shape.perimeter());
-        System.out.println(shape.Avgside());
-        System.out.println(shape.Longestside());
+        System.out.println("Perimeter: " + shape.perimeter());
+        System.out.println("Average side: " + shape.Avgside());
+        System.out.println("Longest side: " + shape.Longestside());
+
+
     }
 }
