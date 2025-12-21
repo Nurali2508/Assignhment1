a4716177de2326d4c55b47d671e87042f6d9cb29
Nurali2508 <250291@astanait.edu.kz>
2025-12-21 21:07:23 +0500

    Update Point.java

--- Models/Point.java
+++ Models/Point.java
@@ -1,16 +1,16 @@
 package Models;
 
 public class Point {
-    private double x;
-    private double y;
-    public Point(double xCrd,double yCrd){
-        x=xCrd;
-        y=yCrd;
+    private double x, y;
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
     }
+
     public double distance(Point p) {
-        double deltaX = this.x - p.x;
-        double deltaY = this.y - p.y;
-        double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+        double dx = this.x - p.x;
+        double dy = this.y - p.y;
+        double distance = Math.sqrt(dx * dx + dy * dy);
         return distance;
     }
 }
